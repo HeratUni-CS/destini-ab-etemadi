6d32b756c81ab24e0223cb9de9db222eddd885cd
ab-etemadi <mr.etmadi@gmail.com>
2022-11-18 20:01:24 +0430

    add nexStory logic

--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -50,19 +50,30 @@ List<Story> _storyData = [
   }
 
 
+
+  int _storyNumber=0;
+
+
+  void nextStory(int choiceNumber){
+    if(_storyNumber == 0 && choiceNumber == 1){
+      _storyNumber= 2;
+    }else if(_storyNumber==0 && choiceNumber==2){
+      _storyNumber= 1;
+    }else if(_storyNumber==1 && choiceNumber==1){
+      _storyNumber= 2;
+    }else if(_storyNumber==1 && choiceNumber==2){
+      _storyNumber= 3;
+    }else if(_storyNumber==2 && choiceNumber==1){
+      _storyNumber= 5;
+    }else if(_storyNumber==2 && choiceNumber==2){
+      _storyNumber= 4;
+    }
+
+  }
+
+
 }
 
-//TODO: Step 23 - Use the storyNumber property inside getStory(), getChoice1() and getChoice2() so that it gets the updated story and choices rather than always just the first (0th) one.
-
-//TODO: Step 25 - Change the storyNumber property into a private property so that only story_brain.dart has access to it. You can do this by right clicking on the name (storyNumber) and selecting Refactor -> Rename to make the change across all the places where it's used.
-
-//TODO: Step 16 - Create a property called storyNumber which starts with a value of 0. This will be used to track which story the user is currently viewing.
-
-//TODO: Step 17 - Create a method called nextStory(), it should not have any outputs but it should have 1 input called choiceNumber which will be the choice number (int) made by the user.
-
-//TODO: Step 20 - The Story Plan should be in your project files, open it
-
-//TODO: Step 21 - Using the story plan, update nextStory() to change the storyNumber depending on the choice made by the user. e.g. if choiceNumber was equal to 1 and the storyNumber is 0, the storyNumber should become 2.
 
 //TODO: Step 22 - In nextStory() if the storyNumber is equal to 3 or 4 or 5, that means it's the end of the game and it should call a method called restart() that resets the storyNumber to 0.
 

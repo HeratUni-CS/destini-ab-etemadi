fce73b7a2ceea1f62ca9f70167f68f36eaf84c6d
ab-etemadi <mr.etmadi@gmail.com>
2022-11-18 19:59:25 +0430

    create class StoryBrain and add its methods

--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -1,7 +1,7 @@
 import 'story.dart';
 
+class StoryBrain {
 
-//TODO: Step 5 - Create a new class called StoryBrain.
 
 List<Story> _storyData = [
  Story(
@@ -35,13 +35,24 @@ List<Story> _storyData = [
      choice2: '')
 ];
 
+  String getStory() {
+    return _storyData[_storyNumber].storyTitle;
+  }
+
+
+  String getChoice1(){
+    return _storyData[_storyNumber].choice1;
+  }
+
+
+  String getChoice2(){
+    return _storyData[_storyNumber].choice2;
+  }
+
+
+}
+
 //TODO: Step 23 - Use the storyNumber property inside getStory(), getChoice1() and getChoice2() so that it gets the updated story and choices rather than always just the first (0th) one.
-
-//TODO: Step 8 - Create a method called getStory() that returns the first storyTitle from _storyData.
-
-//TODO: Step 11 - Create a method called getChoice1() that returns the text for the first choice1 from _storyData.
-
-//TODO: Step 12 - Create a method called getChoice2() that returns the text for the first choice2 from _storyData.
 
 //TODO: Step 25 - Change the storyNumber property into a private property so that only story_brain.dart has access to it. You can do this by right clicking on the name (storyNumber) and selecting Refactor -> Rename to make the change across all the places where it's used.
 
